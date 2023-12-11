e9d3e078a990ecb34cdeaa5542224e77db130d8c
yohanesyudha1908@gmail.com <yohanesyudha1908@gmail.com>
2023-12-11 20:25:09 +0700

    [BAB 9] Firebase

--- src/Jendela/EditData/index.js
+++ src/Jendela/EditData/index.js
@@ -1,4 +1,4 @@
-import {useState, React} from 'react';
+import {useState, React, useEffect, useRoute} from 'react';
 import { Modal, Alert,TouchableOpacity, Button, ScrollView, StyleSheet,  Text, TextInput, View, Image, ImageBackground} from 'react-native';
 import { ShoppingCart,Back,Bag,Notification, Receipt21, Clock, Message, SearchNormal1, RulerPen, Category, Book1, TicketDiscount, BagCross, CloseCircle, Money, Money2, Money4, Money3, ImportSquare, DocumentSketch, Note, Subtitle, Okru, Check, LikeShapes, PasswordCheck, TransmitSquare, AddSquare} from 'iconsax-react-native';
 import { fontType, colors } from '../../theme';
@@ -6,9 +6,31 @@ import {ContentBook, ContentRuler } from '../../IsiKonten';
 import {useNavigation} from '@react-navigation/native';
 import axios from 'axios';
 
-export default function TambahData() {
+const EditData = ({route}) => {
+    
+    const { id } = route.params;
 const navigation = useNavigation();
 const [loading, setLoading] = useState(false);
+
+const [dataBarang, setDataBarang] = useState({
+    name: " ",
+    price: " ",
+    image ,
+    description:" ",
+    createdAt: new Date(),
+  });
+  const handleChange = (key, value) => {
+    setDataBarang({
+      ...dataBarang,
+      [key]: value,
+    });
+  };
+  const [image, setGambar] = useState(null);
+
+useEffect(() => {
+    getBarangById();
+}, [id]);
+
 const handleUpload = async () => {
   setLoading(true);
   try {
@@ -42,12 +64,52 @@ const handleUpload = async () => {
       [key]: value,
     });
   };
-  const [image, setGambar] = useState(null);
+  const getBarangById = async () => {
+    try {
+      const response = await axios.get(
+        `https://65715f5bd61ba6fcc012594d.mockapi.io/DotATK/barang/${id}`,
+      );
+      setDataBarang({
+        name : response.data.nama,
+        price : response.data.harga,
+        image,
+        description: response.data.deskripsi,
+      })
+      setGambar(response.data.image)
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const handleUpdate = async () => {
+    setLoading(true);
+    try {
+      await axios
+        .put(`https://65715f5bd61ba6fcc012594d.mockapi.io/DotATK/barang/${id}`, {
+            dataBarang,
+            name : dataBarang.nama,
+            price : dataBarang.harga,
+            image,
+            description: dataBarang.deskripsi,
+        })
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+      setLoading(false);
+      navigation.navigate('DataBarang');
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
   <View style={styles.container}>
     <View style={styles.tempatlogo}>
       <Back style={{ marginLeft: 10, marginTop:5, }} color={colors.white()} variant="Linear" size={40}/> 
-            <Text style={{marginLeft: -20, fontFamily:fontType['Pjs-Bold'],color:'#FFFFFF', fontSize:25}} >Tambah Data</Text>
+            <Text style={{marginLeft: -20, fontFamily:fontType['Pjs-Bold'],color:'#FFFFFF', fontSize:25}} >Edit Data</Text>
           <AddSquare style={{ marginLeft: -20, marginRight:15, marginTop:5, }} color={colors.white()} variant="Linear" size={40}/>          
     </View>
     <View style={styles.container2}>
@@ -65,8 +127,8 @@ const handleUpload = async () => {
                 <Book1 style={{marginLeft:10, marginRight:10,  marginTop:-4}}color={colors.grey()} variant="Linear" size={20}/>
                 <TextInput
                     placeholder="Nama Barang"
-                    value={form.nama}
-                    onChangeText={(text) => handleForm("nama", text)}
+                    value={dataBarang.name}
+                    onChangeText={(text) => handleChange("nama", text)}
                     placeholderTextColor={colors.grey(0.6)}
                     style={{color: 'black', fontSize:15, marginTop:-5,  width:200,   }}
                 />
@@ -78,8 +140,8 @@ const handleUpload = async () => {
              <Money3 style={{marginLeft:10, marginRight:10,  marginTop:-4}}color={colors.grey()} variant="Linear" size={20}/>
                 <TextInput
                     placeholder="Harga"
-                    value={form.harga}
-                    onChangeText={(text) => handleForm("harga", text)}
+                    value={dataBarang.price}
+                    onChangeText={(text) => handleChange("harga", text)}
                     placeholderTextColor={colors.grey(0.6)}
                     style={{color: 'black', fontSize:15, marginTop:-5,  width:200,    }}
                 />
@@ -104,8 +166,8 @@ const handleUpload = async () => {
              <Note style={{marginLeft:10, marginRight:10,  marginTop:-4}}color={colors.grey()} variant="Linear" size={20}/>
                 <TextInput
                     placeholder="Deskripsi"
-                    value={form.deskripsi}
-                    onChangeText={(text) => handleForm("deskripsi", text)}
+                    value={dataBarang.deskripsi}
+                    onChangeText={(text) => handleChange("deskripsi", text)}
                     placeholderTextColor={colors.grey(0.6)}
                     multiline
                     style={{color: 'black', fontSize:15, marginTop:-5, width:200,}}
@@ -113,7 +175,7 @@ const handleUpload = async () => {
              </View>
         </View>
         <TouchableOpacity
-        onPress={handleUpload}>
+        onPress={handleUpdate}>
         <View style={styles.containerButton}>
           <Text style={{marginLeft: -5, marginBottom:10, marginTop:-15, fontSize: 15, color: '#000000', fontFamily:fontType['Pjs-Bold'] }} >Submit</Text>
           <TransmitSquare style={{  marginTop:-15, marginLeft:60, }} color={colors.black()} variant="Linear" size={30}/>
@@ -122,8 +184,9 @@ const handleUpload = async () => {
       </ScrollView>
     </View>
   </View> 
-  )
-}
+  );
+};
+export default EditData;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
--- src/navigation/Router.js
+++ src/navigation/Router.js
@@ -6,7 +6,9 @@ import Checkout from '../Jendela/Checkout';
 import Bookmark from '../Jendela/Bookmark';
 import Cart from '../Jendela/Cart';
 import TambahData from '../Jendela/TambahData';
-import {ShoppingCart, Home2, LocationDiscover, Receipt21, ProfileCircle, AddSquare} from 'iconsax-react-native'; 
+import DataBarang from '../Jendela/DataBarang';
+import EditData from '../Jendela/EditData';
+import {ShoppingCart, Home2, LocationDiscover, Receipt21, ProfileCircle, AddSquare, ShoppingBag} from 'iconsax-react-native'; 
 import { fontType, colors } from '../theme';
 
 const Tab = createBottomTabNavigator();
@@ -75,6 +77,21 @@ function MainApp() {
           headerShown: false,
         }}
       />
+      <Tab.Screen
+        name="DataBarang "
+        component={DataBarang}
+        options={{
+          tabBarLabel: 'DataBarang',
+          tabBarIcon: ({focused, color}) => (
+            <ShoppingBag
+              color={color}
+              variant={focused ? 'Bold' : 'Linear'}
+              size={24}
+            />
+          ),
+          headerShown: false,
+        }}
+      />
        <Tab.Screen
         name="TambahData"
         component={TambahData}
@@ -103,8 +120,20 @@ const Router = () => {
         options={{headerShown: false}}
       />
       <Stack.Screen
-        name="Checkout"
-        component={Checkout}
+        name="DataBarang"
+        component={DataBarang}
+        options={{
+          headerShown: false, 
+          animationEnabled: true,
+          animationTypeForReplace: 'pop',
+          gestureEnabled: true,
+          gestureDirection : 'horizontal',
+          ...TransitionPresets.SlideFromRightIOS,
+        }}
+      />
+      <Stack.Screen
+        name="EditData"
+        component={EditData}
         options={{
           headerShown: false, 
           animationEnabled: true,
